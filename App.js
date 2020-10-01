6b6af2835841de98e3be2d974a06ac2105fb2e3c
Mark Newman <mwnewman@umich.edu>
2020-09-30 20:48:30 -0400

    renamed ExternalStyles.js

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import { externalStyles } from './ExternalStyle';
+import { externalStyles } from './ExternalStyles';
 
 export function App1() {
   return (
